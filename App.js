2dabdc6643b6edc0303b2dd35245fb156728ac74
satyan3805 <satyan3805@gmail.com>
2023-10-09 06:58:06 +0530

    JSX and Components in React

--- App.js
+++ App.js
@@ -56,4 +56,120 @@ rootReact.render(jsxHeading)
 //Single line JSX
 var jsxHeading = <h1 id="jsxHeading">Heading from JSX!!</h1>;
 //multiline JSX need t wrap in paranthesis()
-var jsxHeading = (<h1 id="jsxHeading">Heading from JSX!!</h1>);
+var jsxHeading = (<h1 
+    id="jsxHeading">Heading from JSX!!
+    </h1>);
+
+//try prettier extension in vscode
+
+//In React every thing is  a compinenet
+//--Functional Componenets(newer way) and -- Classbased componenets(old method).
+
+//React Functional Componenet is nothing but a JS function must return React Element (JSX).
+// React Component name mut be start with CAPITAL Letter.
+
+// Arrow function syntax
+const HeadingComponent = () => {
+    return <h1>ITs a React HeadingComponent</h1>;
+}
+
+//we can skip retun when there is only one line or return.
+const HeadingComponent1 = () => {
+     <h1>ITs a React Head Component1</h1>;
+}
+
+//we can remove the braces aswell when there is only one line or return.
+const HeadingComponent2 = () => <h1>ITs a React Head Component2</h1>;
+
+//we can add paranthesis when there are multiple lines or return.
+const HeadingComponent3 = () => (<h1>ITs a React Head Component3</h1>);
+
+//all the above are sysntaxes or JS not React
+
+// best practice is to use return.
+
+// the JS function can return a nested react elements
+const HeadingComponent4 = () => {
+    <div id="container1">
+        <h1>ITs a React Head Component4</h1>;
+    </div>
+}
+
+// React Element
+const heading = (
+    <h1 className="head" tabIndex="2">ITs a React Element</h1>
+);
+
+//To convert React Element to React Componenet
+//change the name of the variable to CAPITAL Letter and make it as a function.
+
+//Rendering React Element
+rootReact.render(heading);
+
+//Rendering React Componenet (must wrap it as a tag) Babel understands it.
+rootReact.render(<HeadingComponent4 />);
+
+//Nested React Componenets 
+
+const Title1 = () => {
+    <div id="container1">
+        <h1>ITs a React Title1 </h1>;
+    </div>
+}
+
+const HeadingComponent5 = () => {
+    // ITs an error to add <Title1> here because function must return only one React Element (may be nested)
+    //<Title1 />
+    <div id="container1">
+        <Title1 />
+        <Title1></Title1>
+        <Title1/>
+        <h1>ITs a React Head Component5</h1>;
+    </div>
+}
+
+//this is called Componenet Composition (just a terminology)
+
+// Can I use normal function in place of Arrow function to create React Componenet?
+// YES!!
+
+const Title2 = function(){
+    return (
+        <div id="container1">
+            <h1>ITs a React Title2 </h1>;
+        </div>
+    );
+}
+
+//but arrow function are industry wide standard.
+
+// React Element inside a React Componenet
+// React Element
+
+const heading1 = (
+    <h1 className="head" tabIndex="2">ITs a React Heading</h1>
+);
+
+const span = (
+    <span>{heading1}</span>
+);
+
+const num1 = 100;
+const HeadingComponent6 = () => {
+    //we need to use {} to include any Javascript code inside a JSX
+    <div id="container1">
+        <Title1 />
+        {num1};
+        {num1 + 200};
+        <h2>{num1 + 300}</h2>;
+        {console.log("I am satya.n.")};
+        {heading1};
+        {Title1()};
+        <h1>ITs a React Head Component6</h1>;
+    </div>
+}
+
+//is it possible to create infinite loop in HTML?
+// why i am using const to create react element or react componenet?
+
+// Componenet inside a Element
